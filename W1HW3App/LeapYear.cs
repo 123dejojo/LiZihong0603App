45ba53ff826cba4f671cc4f18c24d62e8c04e5b0
JSHY <JSHY@LAPTOP-1Q5LUAC0>
2022-03-02 14:33:35 +0800

    Add project files.

---
+++ W1HW3App/LeapYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW3App
+{
+    internal class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Year :");
+            int year = Convert.ToInt32(Console.ReadLine());
+            bool b = year%400==0 || (year%4==0&& year%100!=0);
+            if (b)
+            {
+                Console.WriteLine($"{year} is a Leap Year.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a Leap Year.");
+            }
+        }
+    }
+}
